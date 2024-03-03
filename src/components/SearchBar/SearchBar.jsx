7369ca6bd277e05759ae1741fe9ce3fbe6093c30
drankstar <137785834+drankstar@users.noreply.github.com>
2024-03-03 21:37:45 +0200

    done

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,18 @@
+import toast, { Toaster } from "react-hot-toast"
 import styles from "./SearchBar.module.css"
 
 const SearchBar = ({ onSubmit }) => {
   const handleSubmit = (e) => {
     e.preventDefault()
+    if (e.target.elements.query.value === "") {
+      toast.error("Please enter a search word!")
+    }
     onSubmit(e.target.elements.query.value)
   }
 
   return (
     <>
+      <Toaster position='top-center' reverseOrder={false} />
       <header className={styles.container}>
         <form className={styles.form} onSubmit={handleSubmit}>
           <input
